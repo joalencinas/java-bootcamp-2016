75661c622da7c5f0a759483a16d220f49821f2c3
joalencinas <lencinasjoaquin@gmail.com>
2016-02-02 03:28:50 -0300

    topic 6 practice 3 progress, still have to write and run tests

---
+++ topic6/practice3/src/main/java/usersAPI/UserRestController.java
@@ -0,0 +1,94 @@
+package usersAPI;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.LinkedList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+    private final UserRepository userRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    ResponseEntity<?> add(@RequestBody User user) {
+        if (!this.userRepository.exists(user.getUsername())) {
+            User result = userRepository.save(new User(user.getUsername(),
+                user.getFirstName(), user.getLastName()));
+            
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setLocation(ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{username}")
+                    .buildAndExpand(result.getUsername()).toUri());
+            return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+            
+        } else {
+            System.out.println("\n\nUsername already taken, choose another one\n\n");
+            return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.IM_USED);
+        }
+        
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    List<User> readUser(@RequestParam(defaultValue="") String username, 
+        @RequestParam(defaultValue="") String firstName, 
+            @RequestParam(defaultValue="") String lastName) {
+        
+        if (username != "") {
+            List<User> result = new LinkedList();
+            result.add(this.userRepository.findOne(username));
+            return result;
+        } else if (firstName != "") {
+            return this.userRepository.findByFirstName(firstName);
+        } else if (lastName != "") {
+            return this.userRepository.findByLastName(lastName);
+        } else {
+            return userRepository.findAll();
+        }
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT)
+    User updateUser(@RequestBody User updatedUser) {
+        User result = userRepository.findOne(updatedUser.getUsername());
+        if (result == null) {
+            throw new RuntimeException("UPDATE FAILED: user not found");
+        }
+        result.setFirstName(updatedUser.getFirstName());
+        result.setLastName(updatedUser.getLastName());
+        return userRepository.save(result);
+    }
+    
+    @RequestMapping(value = "/{username}", method = RequestMethod.DELETE)
+    void deleteUser(@PathVariable String username) {
+        User result = userRepository.findOne(username);
+        if (result == null) {
+            throw new RuntimeException("DELETE FAILED: user not found");
+        }
+        userRepository.delete(result);
+    }
+
+
+    @Autowired
+    UserRestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
